f1bf6185c656e04059098b56d51a41d26bb71b32
YoelEPark <ypark@udesa.edu.ar>
2022-04-04 16:57:55 -0300

    components

---
+++ react-2022/Components/Content/Content.js
@@ -0,0 +1,24 @@
+import React,{Component} from "react";
+
+class Content extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            
+        }
+
+    }
+
+    componentDidMount(){
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US`)
+        .then(response => response.json())
+        .then( data=> {
+            
+        })
+        .catch( error => console.log(error));
+    }
+}
+
+
+
+export default Content;
