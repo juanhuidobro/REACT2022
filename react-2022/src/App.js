2217223cb495f571680f72e3df41ee736607ea0d
YoelEPark <ypark@udesa.edu.ar>
2022-04-11 14:50:42 -0300

    cambios

--- react-2022/src/App.js
+++ react-2022/src/App.js
@@ -1,14 +1,13 @@
-import Header from "./Components/Header/Header";
-import Footer from "./Components/footer/Footer";
+
 import Content from "./Components/Content/Content";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+     
       <Content />
-      <Footer />
+      
 
       
     </div>
--- react-2022/src/Components/Content/Content.js
+++ react-2022/src/Components/Content/Content.js
@@ -6,7 +6,9 @@ class Content extends Component{
     constructor(props){
         super(props)
         this.state = {
-          
+          peliculas:[],
+          charactersBkp:[],
+          nextpage:''
             
         }
 
@@ -15,17 +17,19 @@ class Content extends Component{
     componentDidMount(){
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US`)
         .then(response => response.json())
-        .then( (info) => {
-            console.log(info.data);
-            this.setState({
+        .then( data => this.setState({
+                peliculas:data.results,
+                charactersBkp: data.results,
+                nextPage:data.info.next,
               
 
-            })
-            
-        })
-        .catch( error => console.log(error));
-    }
+            }))
+        .catch( error => console.log(error))
+        }
+    
+
 }
+
 
 
 
