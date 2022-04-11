d4efa6d1c3304004843d43a2080e70a4a20120d7
YoelEPark <ypark@udesa.edu.ar>
2022-04-11 16:46:45 -0300

    empezo a funcionar

--- react-2022/src/Components/Card/Card.js
+++ react-2022/src/Components/Card/Card.js
@@ -1,27 +1,21 @@
 import React,{Component} from "react";
 
+
 class Card extends Component{
     constructor(props){
         super(props)
-        this.state = {
-            
-        }
+     
     }
+    
+     
     render() {
-        return(
-            <div className="card-container">
-                {/* imagen de la pelicula */}
-                    <div className="image-container">
-                      <img className="image" src={this.props.dataPeliculas.cover_big} alt="" />
-                    </div>
-                {/* titulo pelicula */}
-                      <h3>{this.props.dataPeliculas.title}</h3>
-                    {/* decripcion de la pelicula */}
-                      <p className="description">{this.props.dataPeliculas.record_type.toUpperCase()}</p>
-               
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h4> {this.props.title} </h4>
+        </div>
+    )
+}
+    
 }
 
 
--- react-2022/src/Components/Content/Content.js
+++ react-2022/src/Components/Content/Content.js
@@ -2,38 +2,42 @@ import React,{Component} from "react";
 import Card from "../Card/Card";
 
 
+
+
 class Content extends Component{
     constructor(props){
         super(props)
         this.state = {
-          peliculas:[],
-          charactersBkp:[],
-          nextpage:''
+         contenido: []
             
         }
 
     }
 
-    componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US`)
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US&page=1')
         .then(response => response.json())
-        .then( data => this.setState({
-                peliculas:data.results,
-                charactersBkp: data.results,
-                nextPage:data.info.next,
-              
-
-            }))
-        .catch( error => console.log(error))
-        }
-
+        .then(data => 
+        
+            this.setState({
+                contenido: data.results
+            })
+            )
+            .catch( error => console.log(error));
+    
+    }
     render (){
+        console.log('renderizando')
         return(
-            <React.Fragment>
-
-            </React.Fragment>
+            <div> 
+           { this.state.contenido.map( (popular,index) => {
+              return <Card key = {index} title = {popular.title} ></Card>
+             
+            } ) }  
+          </div>
         )
     }
+ 
 
 }
 
